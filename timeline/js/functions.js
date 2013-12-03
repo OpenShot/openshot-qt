de09bdf75f94092db2503e1ba97302ed180d88f9
Cody Parker <cody@yourcodepro.com>
2013-12-03 14:14:41 -0600

    Fixed issues with dragging when the timeline is scrolled to the right/left. Also fixed other drag/drop bugs related with multi-drag. Fixed audio wave rendering to be correct instead of incorrect, which is not preferred. Added in clip element hiding based on clip width after resize.

--- timeline/js/functions.js
+++ timeline/js/functions.js
@@ -7,6 +7,50 @@ function findElement(arr, propName, propValue) {
 
 }
 
+
+//draws the audio wave on a clip
+function drawAudio(scope, element){
+    //hide images and show audio container
+    element.find(".thumb-container").hide();
+    element.find(".audio-container").show();
+    
+    //draw audio
+    var audio_canvas = element.find(".audio");
+    var ctx = audio_canvas[0].getContext('2d');
+    //set the midpoint
+    var mid_point = parseInt(audio_canvas.css("height")) / 2;
+    var line_spot = 0;
+    
+    //draw midpoint line
+    ctx.beginPath();
+    ctx.lineWidth = .5;
+    ctx.beginPath();
+    ctx.moveTo(0, mid_point);
+    ctx.lineTo(parseInt(audio_canvas.css("width")), mid_point);
+    ctx.strokeStyle = "#fff";
+    ctx.stroke();
+
+    //for each point of audio data, draw a line
+    for (var i = 0; i < scope.clip.audio_data.length; i++) {
+        //increase the 'x' axis draw point
+        line_spot += 1;
+        ctx.beginPath();
+        ctx.lineWidth = 1;
+        ctx.beginPath();
+        ctx.moveTo(line_spot, mid_point);
+        var audio_point = scope.clip.audio_data[i];
+        //set the point to draw to
+        var draw_to = (audio_point * mid_point);
+        //handle the 'draw to' point based on positive or negative audio point
+        if (audio_point >= 0) draw_to = mid_point - draw_to;
+        if (audio_point < 0) draw_to = mid_point + (draw_to * -1);
+        //draw it
+        ctx.lineTo(line_spot, draw_to);
+        ctx.strokeStyle = "#FF6E97";
+        ctx.stroke();
+    }
+}
+
 function secondsToTime(secs)
 {
     var t = new Date(1970,0,1);
