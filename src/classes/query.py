10c769b5fbbae1d33447032b1ebd651dafe3c40f
Jonathan Thomas <jonathan@openshot.org>
2014-03-10 22:57:42 -0500

    Implemented query classes, to quickly search for files or clips, based on any number of parameters

--- src/classes/query.py
+++ src/classes/query.py
@@ -31,20 +31,87 @@ from classes.updates import UpdateInterface
 from classes import info, settings
 from classes.logger import log
 from classes.app import get_app
+		
+# Get project data reference
+project = get_app().project
 
-class Clip():
+class QueryObject:
 	""" This class allows one or more project data objects to be queried """
-	
-	def __init__(self, **kwargs):
+
+	def __init__(self):
 		""" Constructor """
 		
-		self.app = get_app()
-		self.project = app.project
-		self.root_key = "clip"
-	
-	def filter(self, **kwargs):
-		""" Take any arguments given as filters, and find a matching object """
+		self.id = None		# Unique ID of object
+		self.key = None		# Key path to object in project data
+		self.data = None	# Data dictionary of object
 		
-		for key, value in kwargs.iteritems():
-			log.info("%s = %s" % (key, value))
+	def save(self):
+		""" Save the object back to the project data store """
+		pass
 
+	def delete(self):
+		""" Delete the object from the project data store """
+		pass
+	
+	def filter(OBJECT_TYPE, **kwargs):
+		""" Take any arguments given as filters, and find a list of matching objects """
+		
+		# Get a list of all objects of this type
+		parent = project.get(OBJECT_TYPE.object_key)
+		matching_objects = []
+		
+		# Loop through all children objects
+		for child in parent:
+			
+			# Loop through all kwargs (and look for matches)
+			match = True
+			for key, value in kwargs.items():
+				if key in child and not child[key] == value:
+					match = False
+					break
+				
+			# Add matched record
+			if match:
+				object = OBJECT_TYPE()
+				object.id = child["id"]
+				object.key = [OBJECT_TYPE.object_name, { "id" : object.id}]
+				object.data = child
+				matching_objects.append(object)
+				
+		# Return matching objects
+		return matching_objects
+				
+	
+	def get(OBJECT_TYPE, **kwargs):
+		""" Take any arguments given as filters, and find the first matching object """
+		
+		# Look for matching objects
+		matching_objects = QueryObject.filter(OBJECT_TYPE, **kwargs)
+		
+		if matching_objects:
+			return matching_objects[0]
+		else:
+			return None
+
+class Clip(QueryObject):
+	""" This class allows one or more project data objects to be queried """
+	object_name = "clips"		# Derived classes should define this
+	object_key = [object_name]	# Derived classes should define this also
+		
+	def save(self):
+		""" Save the object back to the project data store """
+		log.info("Clip save method")
+
+	def delete(self):
+		""" Delete the object from the project data store """
+		log.info("Clip delete method")
+
+	def filter(**kwargs):
+		""" Take any arguments given as filters, and find a list of matching objects """
+		return QueryObject.filter(Clip, **kwargs)
+		
+	def get(**kwargs):
+		""" Take any arguments given as filters, and find the first matching object """
+		return QueryObject.get(Clip, **kwargs)
+	
+				
