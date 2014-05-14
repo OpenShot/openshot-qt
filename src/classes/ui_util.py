78deeae3511015a906de0b4874f78c21c780c571
Jonathan Thomas <jonathan@openshot.org>
2014-05-13 22:49:00 -0500

    Fixed stupid regression.

--- src/classes/ui_util.py
+++ src/classes/ui_util.py
@@ -40,7 +40,14 @@ DEFAULT_THEME_NAME = "Humanity"
 def load_theme():
 	""" Load the current OS theme, or fallback to a default one """
 	
-	QIcon.setThemeName(DEFAULT_THEME_NAME)
+	#If theme not reported by OS
+	if QIcon.themeName() == '':
+		#Address known Ubuntu bug of not reporting configured theme name, use default ubuntu theme
+		if os.getenv('DESKTOP_SESSION') == 'ubuntu':
+			QIcon.setThemeName('unity-icon-theme')
+		#Windows/Mac use packaged theme
+		else:
+			QIcon.setThemeName(DEFAULT_THEME_NAME)
 			
 def load_ui(window, path):
 	""" Load a Qt *.ui file, and also load an XML parsed version """ 
