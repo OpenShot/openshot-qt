483fe05d80103a2f5628acc60881f4a213808dab
Jonathan Thomas <jonathan@openshot.org>
2016-03-22 17:44:01 -0500

    Improved build server: better file naming (combing libopenshot, libopenshot-audio, and openshot-qt GIT meta data). Only build when needed. Only upload when needed. Don't re-upload a file if it's been uploaded successfully already.

--- installer/build-server.py
+++ installer/build-server.py
@@ -56,6 +56,7 @@ s3_secret_key = None
 s3_connection = None
 windows_key = None
 windows_key_password = None
+commit_data = {}
 
 
 # Determine the paths and cmake args for each platform
@@ -116,7 +117,7 @@ def upload(file_path, s3_bucket):
             except Exception as ex:
                 # Quietly fail, and try again
                 if attempt < 2:
-                    output("Amazon S3 upload failed... tring again")
+                    output("Amazon S3 upload failed... trying again")
                 else:
                     # Throw loud exception
                     raise ex
@@ -142,6 +143,11 @@ try:
     for project_path, cmake_args in project_paths:
         # Change os directory
         os.chdir(project_path)
+
+        # Get number of commits
+        for line in run_command("git rev-list --count master"):
+            commit_data[project_path] = int(line)
+            output("# of commits found for %s: %s" % (project_path, commit_data[project_path]))
 
         # Check for new version in git
         needs_update = True
@@ -192,11 +198,20 @@ try:
                     error("Make Install Error: %s" % line)
 
 
-    # Now that everything is compiled, let's create the installers
-    if not errors_detected and needs_build:
+    # Now that everything is updated, let's create the installers
+    if not errors_detected:
         # Change to openshot-qt dir
         project_path = project_paths[2][0]
         os.chdir(project_path)
+
+        # Get GIT description of openshot-qt-git branch (i.e. v2.0.6-18-ga01a98c)
+        openshot_qt_git_desc = ""
+        for line in run_command("git describe --tags"):
+            openshot_qt_git_desc = line.decode("utf-8").replace("\n","")
+            output("git description of openshot-qt-git: %s" % openshot_qt_git_desc)
+
+            # Add num of commits from libopenshot and libopenshot-audio (for naming purposes)
+            openshot_qt_git_desc = "%s-%s-%s" % (openshot_qt_git_desc, commit_data[project_paths[0][0]], commit_data[project_paths[1][0]])
 
         # Check for left over openshot-qt dupe folder
         if os.path.exists(os.path.join(project_path, "openshot_qt")):
@@ -206,162 +221,174 @@ try:
         if os.path.exists(os.path.join(project_path, "dist")):
             shutil.rmtree(os.path.join(project_path, "dist"))
 
-        # Freeze it
-        for line in run_command(freeze_command):
-            output(line)
-            if "logger:ERROR".encode("UTF-8") in line and not "importlib/__init__.pyc".encode("UTF-8") in line and not "zinfo".encode("UTF-8") in line:
-                error("Freeze Error: %s" % line)
+        # Create uploads path (if not found)
+        upload_path = os.path.join(project_path, "s3-uploads")
+        if not os.path.exists(upload_path):
+            os.mkdir(upload_path)
+        builds_path = os.path.join(project_path, "s3-builds")
+        if not os.path.exists(builds_path):
+            os.mkdir(builds_path)
 
-        # Successfully compiled - Time to create installers
+        # Determine the name and path of the final installer
+        app_name = openshot_qt_git_desc
+        app_upload_bucket = ""
         if platform.system() == "Linux":
+            app_name += "-x86_64.AppImage"
+            app_upload_bucket = "releases.openshot.org/linux"
+        elif platform.system() == "Darwin":
+            app_name += "-x86_64.dmg"
+            app_upload_bucket = "releases.openshot.org/mac"
+        elif platform.system() == "Windows":
+            app_name += "-x86_32.msi"
+            app_upload_bucket = "releases.openshot.org/windows"
+        app_build_path = os.path.join(builds_path, app_name)
+        app_upload_path = os.path.join(upload_path, app_name)
 
-            # Find exe folder
-            exe_dirs = os.listdir(os.path.join(project_path, "build"))
-            if len(exe_dirs) == 1:
-                exe_dir = exe_dirs[0]
+        # Does this version need to be built?
+        needs_build = False
+        if not os.path.exists(app_build_path) and not os.path.exists(app_upload_path):
+            needs_build = True
 
-                # Create AppDir folder
-                app_dir_path = os.path.join(project_path, "build", "OpenShot.AppDir")
-                os.mkdir(app_dir_path)
-                os.mkdir(os.path.join(app_dir_path, "usr"))
-                os.mkdir(os.path.join(app_dir_path, "usr", "share"))
-                os.mkdir(os.path.join(app_dir_path, "usr", "share", "pixmaps"))
-                os.mkdir(os.path.join(app_dir_path, "usr", "share", "mime"))
-                os.mkdir(os.path.join(app_dir_path, "usr", "share", "mime", "packages"))
-                os.mkdir(os.path.join(app_dir_path, "usr", "lib"))
-                os.mkdir(os.path.join(app_dir_path, "usr", "lib", "mime"))
-                os.mkdir(os.path.join(app_dir_path, "usr", "lib", "mime", "packages"))
+        # Does this version need to be uploaded?
+        needs_upload = False
+        if not os.path.exists(app_upload_path):
+            needs_upload = True
 
-                # Create AppRun file
-                app_run_path = os.path.join(app_dir_path, "AppRun")
-                shutil.copyfile("/home/jonathan/apps/AppImageKit/AppRun", app_run_path)
 
-                # Create .desktop file
-                with open(os.path.join(app_dir_path, "openshot-qt.desktop"), "w") as f:
-                    f.write('[Desktop Entry]\nName=OpenShot Video Editor\nGenericName=Video Editor\nX-GNOME-FullName=OpenShot Video Editor\nComment=Create and edit amazing videos and movies\nExec=openshot-qt.wrapper %F\nTerminal=false\nIcon=openshot-qt\nType=Application')
+        if needs_build:
+            # Freeze it (if needed)
+            for line in run_command(freeze_command):
+                output(line)
+                if "logger:ERROR".encode("UTF-8") in line and not "importlib/__init__.pyc".encode("UTF-8") in line and not "zinfo".encode("UTF-8") in line:
+                    error("Freeze Error: %s" % line)
 
-                # Copy some installation-related files
-                shutil.copyfile(os.path.join(project_path, "xdg", "openshot-qt.svg"), os.path.join(app_dir_path, "openshot-qt.svg"))
-                shutil.copyfile(os.path.join(project_path, "xdg", "openshot-qt.svg"), os.path.join(app_dir_path, "usr", "share", "pixmaps", "openshot-qt.svg"))
-                shutil.copyfile(os.path.join(project_path, "xdg", "openshot-qt.xml"), os.path.join(app_dir_path, "usr", "share", "mime", "packages", "openshot-qt.xml"))
-                shutil.copyfile(os.path.join(project_path, "xdg", "openshot-qt"), os.path.join(app_dir_path, "usr", "lib", "mime", "packages", "openshot-qt"))
+            # Successfully frozen - Time to create installers
+            if platform.system() == "Linux":
 
-                # Copy the entire frozen app
-                shutil.copytree(os.path.join(project_path, "build", exe_dir), os.path.join(app_dir_path, "usr", "bin"))
+                # Find exe folder
+                exe_dirs = os.listdir(os.path.join(project_path, "build"))
+                if len(exe_dirs) == 1:
+                    exe_dir = exe_dirs[0]
 
-                # Copy desktop integration wrapper (prompts users to install shortcut)
-                launcher_path = os.path.join(app_dir_path, "usr", "bin", "openshot-qt")
-                os.rename(os.path.join(app_dir_path, "usr", "bin", "launch-linux.sh"), launcher_path)
-                desktop_wrapper = os.path.join(app_dir_path, "usr", "bin", "openshot-qt.wrapper")
-                shutil.copyfile("/home/jonathan/apps/AppImageKit/desktopintegration", desktop_wrapper)
+                    # Create AppDir folder
+                    app_dir_path = os.path.join(project_path, "build", "OpenShot.AppDir")
+                    os.mkdir(app_dir_path)
+                    os.mkdir(os.path.join(app_dir_path, "usr"))
+                    os.mkdir(os.path.join(app_dir_path, "usr", "share"))
+                    os.mkdir(os.path.join(app_dir_path, "usr", "share", "pixmaps"))
+                    os.mkdir(os.path.join(app_dir_path, "usr", "share", "mime"))
+                    os.mkdir(os.path.join(app_dir_path, "usr", "share", "mime", "packages"))
+                    os.mkdir(os.path.join(app_dir_path, "usr", "lib"))
+                    os.mkdir(os.path.join(app_dir_path, "usr", "lib", "mime"))
+                    os.mkdir(os.path.join(app_dir_path, "usr", "lib", "mime", "packages"))
 
-                # Change permission of AppRun (and desktop.wrapper) file (add execute permission)
-                st = os.stat(app_run_path)
-                os.chmod(app_run_path, st.st_mode | stat.S_IEXEC)
-                os.chmod(desktop_wrapper, st.st_mode | stat.S_IEXEC)
-                os.chmod(launcher_path, st.st_mode | stat.S_IEXEC)
+                    # Create AppRun file
+                    app_run_path = os.path.join(app_dir_path, "AppRun")
+                    shutil.copyfile("/home/jonathan/apps/AppImageKit/AppRun", app_run_path)
 
-                # Create AppImage (OpenShot-%s-x86_64.AppImage)
+                    # Create .desktop file
+                    with open(os.path.join(app_dir_path, "openshot-qt.desktop"), "w") as f:
+                        f.write('[Desktop Entry]\nName=OpenShot Video Editor\nGenericName=Video Editor\nX-GNOME-FullName=OpenShot Video Editor\nComment=Create and edit amazing videos and movies\nExec=openshot-qt.wrapper %F\nTerminal=false\nIcon=openshot-qt\nType=Application')
+
+                    # Copy some installation-related files
+                    shutil.copyfile(os.path.join(project_path, "xdg", "openshot-qt.svg"), os.path.join(app_dir_path, "openshot-qt.svg"))
+                    shutil.copyfile(os.path.join(project_path, "xdg", "openshot-qt.svg"), os.path.join(app_dir_path, "usr", "share", "pixmaps", "openshot-qt.svg"))
+                    shutil.copyfile(os.path.join(project_path, "xdg", "openshot-qt.xml"), os.path.join(app_dir_path, "usr", "share", "mime", "packages", "openshot-qt.xml"))
+                    shutil.copyfile(os.path.join(project_path, "xdg", "openshot-qt"), os.path.join(app_dir_path, "usr", "lib", "mime", "packages", "openshot-qt"))
+
+                    # Copy the entire frozen app
+                    shutil.copytree(os.path.join(project_path, "build", exe_dir), os.path.join(app_dir_path, "usr", "bin"))
+
+                    # Copy desktop integration wrapper (prompts users to install shortcut)
+                    launcher_path = os.path.join(app_dir_path, "usr", "bin", "openshot-qt")
+                    os.rename(os.path.join(app_dir_path, "usr", "bin", "launch-linux.sh"), launcher_path)
+                    desktop_wrapper = os.path.join(app_dir_path, "usr", "bin", "openshot-qt.wrapper")
+                    shutil.copyfile("/home/jonathan/apps/AppImageKit/desktopintegration", desktop_wrapper)
+
+                    # Change permission of AppRun (and desktop.wrapper) file (add execute permission)
+                    st = os.stat(app_run_path)
+                    os.chmod(app_run_path, st.st_mode | stat.S_IEXEC)
+                    os.chmod(desktop_wrapper, st.st_mode | stat.S_IEXEC)
+                    os.chmod(launcher_path, st.st_mode | stat.S_IEXEC)
+
+                    # Create AppImage (OpenShot-%s-x86_64.AppImage)
+                    app_image_success = False
+                    for line in run_command('/home/jonathan/apps/AppImageKit/AppImageAssistant "%s" "%s"' % (app_dir_path, app_build_path)):
+                        output(line)
+                        if "error".encode("UTF-8") in line:
+                            error("AppImageKit Error: %s" % line)
+                        if "completed sucessfully".encode("UTF-8") in line:
+                            app_image_success = True
+
+                    # Was the AppImage creation successful
+                    if not app_image_success:
+                        # AppImage failed
+                        error("AppImageKit Error: AppImageAssistant did not output 'completed successfully'")
+                        needs_upload = False
+
+
+            if platform.system() == "Darwin":
+
+                # Create DMG (OpenShot-%s-x86_64.DMG)
                 app_image_success = False
-                app_name = "OpenShot-%s-%s-x86_64.AppImage" % (info.VERSION, datetime.datetime.now().strftime("%Y-%m-%d"))
-                app_image_path = os.path.join(project_path, "build", app_name)
-                for line in run_command('/home/jonathan/apps/AppImageKit/AppImageAssistant "%s" "%s"' % (app_dir_path, app_image_path)):
+
+                # Build app.bundle and create DMG
+                for line in run_command("bash installer/build-mac-dmg.sh"):
                     output(line)
                     if "error".encode("UTF-8") in line:
-                        error("AppImageKit Error: %s" % line)
-                    if "completed sucessfully".encode("UTF-8") in line:
+                        error("Build-Mac-DMG Error: %s" % line)
+                    if "Your image is ready".encode("UTF-8") in line:
                         app_image_success = True
 
-                # Was the AppImage creation successful
+                # Was the DMG creation successful
                 if app_image_success:
-                    # Check if AppImage exists
-                    if os.path.exists(app_image_path):
-                        # Upload file to S3
-                        output("S3: Uploading %s to Amazon S3" % app_image_path)
-                        upload(app_image_path, "releases.openshot.org/linux")
-
-                        # Notify Slack
-                        slack("%s: Successful build: http://releases.openshot.org/linux/%s" % (platform.system(), app_name))
-
-                    else:
-                        # AppImage doesn't exist
-                        error("AppImageKit Error: %s does not exist" % app_image_path)
-                else:
-                    # AppImage failed
-                    error("AppImageKit Error: AppImageAssistant did not output 'completed successfully'")
-
-
-        if platform.system() == "Darwin":
-
-            # Create DMG (OpenShot-%s-x86_64.DMG)
-            app_image_success = False
-            app_name = "OpenShot-%s-%s-x86_64.dmg" % (info.VERSION, datetime.datetime.now().strftime("%Y-%m-%d"))
-            app_image_path = os.path.join(project_path, "build", app_name)
-
-            # Build app.bundle and create DMG
-            for line in run_command("bash installer/build-mac-dmg.sh"):
-                output(line)
-                if "error".encode("UTF-8") in line:
-                    error("Build-Mac-DMG Error: %s" % line)
-                if "Your image is ready".encode("UTF-8") in line:
-                    app_image_success = True
-
-            # Was the DMG creation successful
-            if app_image_success:
-                # Rename DMG (to be consistent with other OS installers)
-                os.rename(os.path.join(project_path, "build", "OpenShot-%s.dmg" % info.VERSION), app_image_path)
-
-                # Check if DMG exists
-                if os.path.exists(app_image_path):
-                    # Upload file to S3
-                    output("S3: Uploading %s to Amazon S3" % app_image_path)
-                    upload(app_image_path, "releases.openshot.org/mac")
-
-                    # Notify Slack
-                    slack("%s: Successful build: http://releases.openshot.org/mac/%s" % (platform.system(), app_name))
+                    # Rename DMG (to be consistent with other OS installers)
+                    os.rename(os.path.join(project_path, "build", "OpenShot-%s.dmg" % info.VERSION), app_build_path)
 
                 else:
-                    # DMG doesn't exist
-                    error("Build-Mac-DMG Error: %s does not exist" % app_image_path)
+                    # DMG failed
+                    error("Build-Mac-DMG Error: Did not output 'Your image is ready'")
+                    needs_upload = False
+
+
+            if platform.system() == "Windows":
+
+                # Create MSI (OpenShot-%s-x86_32.MSI)
+                app_image_success = True
+
+                # Rename MSI (to be consistent with other OS installers)
+                os.rename(os.path.join(project_path, "dist", "OpenShot Video Editor-%s-win32.msi" % info.VERSION), app_build_path)
+
+                key_sign_command = '"C:\\Program Files (x86)\\kSign\\kSignCMD.exe" /f "%s" /p "%s" /d "OpenShot Video Editor" /du "http://www.openshot.org" "%s"' % (windows_key, windows_key_password, app_build_path)
+                # Sign MSI
+                for line in run_command(key_sign_command):
+                    output(line)
+                    if line:
+                        app_image_success = False
+
+                # Was the MSI creation successful
+                if not app_image_success:
+                    # MSI failed
+                    error("Key Sign Error: Had output when none was expected")
+                    needs_upload = False
+
+
+        if needs_upload:
+            # Check if app exists
+            if os.path.exists(app_build_path):
+                # Upload file to S3
+                output("S3: Uploading %s to Amazon S3" % app_build_path)
+                upload(app_build_path, app_upload_bucket)
+
+                # Notify Slack
+                slack("%s: Successful build: http://%s/%s" % (platform.system(), app_upload_bucket, app_name))
+
+                # Copy app to uploads folder (so it will be skipped next time)
+                shutil.copyfile(app_build_path, app_upload_path)
+
             else:
-                # DMG failed
-                error("Build-Mac-DMG Error: Did not output 'Your image is ready'")
-
-
-        if platform.system() == "Windows":
-
-            # Create MSI (OpenShot-%s-x86_32.MSI)
-            app_image_success = True
-            app_name = "OpenShot-%s-%s-x86_32.msi" % (info.VERSION, datetime.datetime.now().strftime("%Y-%m-%d"))
-            app_image_path = os.path.join(project_path, "dist", app_name)
-
-            # Rename MSI (to be consistent with other OS installers)
-            os.rename(os.path.join(project_path, "dist", "OpenShot Video Editor-%s-win32.msi" % info.VERSION), app_image_path)
-
-            key_sign_command = '"C:\\Program Files (x86)\\kSign\\kSignCMD.exe" /f "%s" /p "%s" /d "OpenShot Video Editor" /du "http://www.openshot.org" "%s"' % (windows_key, windows_key_password, app_image_path)
-            # Sign MSI
-            for line in run_command(key_sign_command):
-                output(line)
-                if line:
-                    app_image_success = False
-
-            # Was the MSI creation successful
-            if app_image_success:
-                # Check if MSI exists
-                if os.path.exists(app_image_path):
-                    # Upload file to S3
-                    output("S3: Uploading %s to Amazon S3" % app_image_path)
-                    upload(app_image_path, "releases.openshot.org/windows")
-
-                    # Notify Slack
-                    slack("%s: Successful build: http://releases.openshot.org/windows/%s" % (platform.system(), app_name))
-
-                else:
-                    # MSI doesn't exist
-                    error("MSI Error: %s does not exist" % app_image_path)
-            else:
-                # MSI failed
-                error("Key Sign Error: Had output when none was expected")
+                # App doesn't exist (something went wrong)
+                error("App Missing Error: %s does not exist" % app_build_path)
 
 
 except Exception as ex:
