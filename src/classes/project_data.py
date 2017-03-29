44300447a1fde154a727c3028ecc0c24433eeb04
Jonathan Thomas <jonathan@openshot.org>
2017-03-29 01:15:12 -0500

    Fixing bug when opening legacy projects (1.4.3) that contain effects or markers

--- src/classes/project_data.py
+++ src/classes/project_data.py
@@ -395,6 +395,8 @@ class ProjectDataStore(JsonDataStore, UpdateInterface):
         from classes.legacy.openshot.classes import keyframe as legacy_keyframe
         from classes.legacy.openshot.classes import files as legacy_files
         from classes.legacy.openshot.classes import transition as legacy_transition
+        from classes.legacy.openshot.classes import effect as legacy_effect
+        from classes.legacy.openshot.classes import marker as legacy_marker
         sys.modules['openshot.classes'] = legacy_classes
         sys.modules['classes.project'] = legacy_project
         sys.modules['classes.sequences'] = legacy_sequences
@@ -403,6 +405,8 @@ class ProjectDataStore(JsonDataStore, UpdateInterface):
         sys.modules['classes.keyframe'] = legacy_keyframe
         sys.modules['classes.files'] = legacy_files
         sys.modules['classes.transition'] = legacy_transition
+        sys.modules['classes.effect'] = legacy_effect
+        sys.modules['classes.marker'] = legacy_marker
 
         # Keep track of files that failed to load
         failed_files = []
