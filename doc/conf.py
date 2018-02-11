b62c74899c22ca2c136ea34b9ccddc8a83f4fde7
FeRD (Frank Dana) <ferdnyc@gmail.com>
2018-02-11 12:31:58 -0500

    Add tablefix.css to enable wrapping in table cells

--- doc/conf.py
+++ doc/conf.py
@@ -155,7 +155,7 @@ html_favicon = "../xdg/openshot-qt.ico"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['images']
+html_static_path = ['images', 'css']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
