2e40151e09d005e2ac8a4f5a894f47cbd6c8156f
FeRD (Frank Dana) <ferdnyc@gmail.com>
2018-03-12 01:08:40 -0400

    Add import_path tracking to project data

--- src/classes/timeline.py
+++ src/classes/timeline.py
@@ -76,7 +76,7 @@ class TimelineSync(UpdateInterface):
         """ This method is invoked by the UpdateManager each time a change happens (i.e UpdateInterface) """
 
         # Ignore changes that don't affect libopenshot
-        if len(action.key) >= 1 and action.key[0].lower() in ["files", "history", "markers", "layers", "export_path", "scale"]:
+        if len(action.key) >= 1 and action.key[0].lower() in ["files", "history", "markers", "layers", "export_path", "import_path", "scale"]:
             return
 
         elif len(action.key) >= 1 and action.key[0].lower() in ["profile"]:
