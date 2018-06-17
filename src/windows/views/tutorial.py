36419e32e9c5ce4d8023f2ac226c208a88c61f6f
Jonathan Thomas <jonathan@openshot.org>
2018-06-17 13:00:37 -0500

    Reverting tutorial.py

--- src/windows/views/tutorial.py
+++ src/windows/views/tutorial.py
@@ -74,16 +74,11 @@ class TutorialDialog(QWidget):
     def eventFilter(self, object, e):
         if e.type() == QEvent.WindowActivate:
             # Raise parent window, and then this tutorial
-            #log.info("Raising main app and tutorial popup")
-            get_app().window.show()
+            get_app().window.showNormal()
             get_app().window.raise_()
-            self.moveWidget()
-            self.show()
             self.raise_()
-            # Filter event out (prevent further handling)
-            return True
-        else:
-            return False
+
+        return False
 
     def moveWidget(self):
         """ Move widget next to its position widget """
@@ -175,9 +170,9 @@ class TutorialDialog(QWidget):
 
         # Make it's own window
         self.setWindowTitle("Tutorial")
-        self.setWindowFlags(Qt.Dialog | Qt.FramelessWindowHint)
+        self.setWindowFlags(Qt.Window | Qt.FramelessWindowHint)
         self.setAttribute(Qt.WA_TranslucentBackground, True)
-        self.setFocusPolicy(Qt.ClickFocus)
+        self.setFocusPolicy(Qt.NoFocus)
 
         # Position window next to other widget
         self.moveWidget()
