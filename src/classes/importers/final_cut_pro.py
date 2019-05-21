dee3eb2fa2caaf656689f00961dfa1b6365dc4f6
Jonathan Thomas <jonathan@openshot.org>
2019-05-21 00:45:16 -0500

    Adding 'resources' folder, and moving some non-settings file there (trying to clean-up the folder structure a bit). Also stubbing out the EDL and XML importers (WIP).

---
+++ src/classes/importers/final_cut_pro.py
@@ -0,0 +1,45 @@
+"""
+ @file
+ @brief This file is used to import a Final Cut Pro XML file
+ @author Jonathan Thomas <jonathan@openshot.org>
+
+ @section LICENSE
+
+ Copyright (c) 2008-2018 OpenShot Studios, LLC
+ (http://www.openshotstudios.com). This file is part of
+ OpenShot Video Editor (http://www.openshot.org), an open-source project
+ dedicated to delivering high quality video editing and animation solutions
+ to the world.
+
+ OpenShot Video Editor is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ OpenShot Video Editor is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with OpenShot Library.  If not, see <http://www.gnu.org/licenses/>.
+ """
+
+import os
+from operator import itemgetter
+
+from PyQt5.QtWidgets import *
+
+from classes import info
+from classes.app import get_app
+from classes.logger import log
+from classes.query import Clip, Track
+from classes.time_parts import secondsToTime
+
+from uuid import uuid1
+from xml.dom import minidom
+
+
+def import_xml():
+    """Import final cut pro XML file"""
+    pass
