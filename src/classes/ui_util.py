e05ea4867ce33613567b9df1907dc7b41ca659f5
FeRD (Frank Dana) <ferdnyc@gmail.com>
2019-08-27 16:00:35 -0400

    classes/ui_util: Remove unused variable
    Co-authored-by: TreOne <tre@tre.one>

--- src/classes/ui_util.py
+++ src/classes/ui_util.py
@@ -192,12 +192,10 @@ def connect_auto_events(window, elem, name):
     if hasattr(elem, 'trigger'):
         func_name = name + "_trigger"
         if hasattr(window, func_name) and callable(getattr(window, func_name)):
-            func = getattr(window, func_name)
             elem.triggered.connect(getattr(window, func_name))
     if hasattr(elem, 'click'):
         func_name = name + "_click"
         if hasattr(window, func_name) and callable(getattr(window, func_name)):
-            func = getattr(window, func_name)
             elem.clicked.connect(getattr(window, func_name))
 
 
