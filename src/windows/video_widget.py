197e4b00256570989f3ab539237140dd9ecf6cee
SuslikV <vx2010@i.ua>
2019-08-31 11:00:38 +0200

    Use theme color for Video Preview background
    Co-authored-by: Frank Dana <ferdnyc@users.noreply.github.com>

--- src/windows/video_widget.py
+++ src/windows/video_widget.py
@@ -82,7 +82,7 @@ class VideoWidget(QWidget, updates.UpdateInterface):
         painter = QPainter(self)
         painter.setRenderHints(QPainter.Antialiasing | QPainter.SmoothPixmapTransform | QPainter.TextAntialiasing, True)
 
-        # Fill background black
+        # Fill background with theme color instead of black
         painter.fillRect(event.rect(), self.palette().window())
 
         if self.current_image:
@@ -607,10 +607,7 @@ class VideoWidget(QWidget, updates.UpdateInterface):
         # Mutex lock
         self.mutex = QMutex()
 
-        # Init Qt style properties (black background, etc...)
-        p = QPalette()
-        p.setColor(QPalette.Window, QColor("#191919"))
-        super().setPalette(p)
+        # Init Qt widjet's properties (background repainting, etc...)
         super().setAttribute(Qt.WA_OpaquePaintEvent)
         super().setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
 
