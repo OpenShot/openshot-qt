7cdbcb8738fdd48d2cfae64a3881646e6cc26177
Frank Dana <ferdnyc@gmail.com>
2019-09-16 21:45:29 -0400

    Whitespace-change commit to trigger GitHub rescan

--- src/windows/title_editor.py
+++ src/windows/title_editor.py
@@ -24,7 +24,7 @@
 
  You should have received a copy of the GNU General Public License
  along with OpenShot Library.  If not, see <http://www.gnu.org/licenses/>.
- """
+"""
 
 import sys
 import os
