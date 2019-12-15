d1f1ccff3e528611a3bd7aa236c57071ad7c02a2
SuslikV <vx2010@i.ua>
2019-12-15 11:48:49 +0200

    Check if playhead follow code is available
    Not all parents of the preview needs (and has) the playhead follow
    code. Cutting dialog window for example doesn't uses browser to set
    playback position.

--- src/windows/preview_thread.py
+++ src/windows/preview_thread.py
@@ -55,10 +55,14 @@ class PreviewParent(QObject):
     # Signal when the playback mode changes in the preview player (i.e PLAY, PAUSE, STOP)
     def onModeChanged(self, current_mode):
         log.info('onModeChanged')
-        if current_mode is openshot.PLAYBACK_PLAY:
-            self.parent.SetPlayheadFollow(False)
-        else:
-            self.parent.SetPlayheadFollow(True)
+        try:
+            if current_mode is openshot.PLAYBACK_PLAY:
+                self.parent.SetPlayheadFollow(False)
+            else:
+                self.parent.SetPlayheadFollow(True)
+        except AttributeError:
+            # Parent object doesn't need the playhead follow code
+            pass
 
     # Signal when the playback encounters an error
     def onError(self, error):
