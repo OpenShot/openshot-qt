accfd6a137bcc6bbfc8986a383f0705f8ae6ff75
Frank Dana <ferdnyc@gmail.com>
2020-03-27 00:02:41 -0400

    Move manual logo to xdg dir (#3320)

--- doc/conf.py
+++ doc/conf.py
@@ -166,7 +166,7 @@ html_theme_path = ["_themes", ]
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
 #
-html_logo = "../src/images/openshot-arrow.png"
+html_logo = "../xdg/openshot-arrow.png"
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
