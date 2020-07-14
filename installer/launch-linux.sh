253d868c926e7ec176a0dea745abf195495cbdb2
Jonathan Thomas <jonathan@openshot.org>
2020-07-14 17:23:01 -0500

    Tweaking launcher bash file, to correctly add the current folder to LD_LIBRARY_PATH

--- installer/launch-linux.sh
+++ installer/launch-linux.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # Add the current folder the library path
-LD_LIBRARY_PATH="."
+HERE="$(dirname "$(readlink -f "${0}")")"
+export LD_LIBRARY_PATH="${HERE}"
 
-# Set some environment variables for ImageMagick and Qt5
-export QT_PLUGIN_PATH="."
+# Set some environment variables
+export QT_PLUGIN_PATH="${HERE}"
 
 # Launch application
-HERE="$(dirname "$(readlink -f "${0}")")"
 exec "${HERE}"/openshot-qt "$@"
