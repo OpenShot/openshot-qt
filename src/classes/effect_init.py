64acae8e81302fcf839dd9882784094a770fa26b
Brenno <brenno.caldato@gmail.com>
2021-03-26 21:43:17 -0300

    Changed default ObjectDetection default path for required files
    ObjectDetection config and weights should be dowloaded to ~/.openshot-qt/yolo/

--- src/classes/effect_init.py
+++ src/classes/effect_init.py
@@ -25,7 +25,8 @@
  You should have received a copy of the GNU General Public License
  along with OpenShot Library.  If not, see <http://www.gnu.org/licenses/>.
  """
-
+from classes.info import USER_PATH
+import os
 # Not all Effects support pre-processing, so for now, this is a hard-coded
 # solution to providing the pre-processing params needed for these special effects.
 
@@ -150,19 +151,19 @@ effect_options = {
 
     "Object Detector": [
         {
-            "value": "../yolo/yolov3.weights",
+            "value": os.path.join(USER_PATH,"yolo/yolov3.weights"),
             "title": "Model Weights",
             "type": "text",
             "setting": "model-weights"
         },
         {
-            "value": "../yolo/yolov3.cfg",
+            "value": os.path.join(USER_PATH,"yolo/yolov3.cfg"),
             "title": "Model Config",
             "type": "text",
             "setting": "model-config"
         },
         {
-            "value": "../yolo/obj.names",
+            "value": os.path.join(USER_PATH,"yolo/obj.names"),
             "title": "Class names",
             "type": "text",
             "setting": "class-names"
