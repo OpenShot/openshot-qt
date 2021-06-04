2034697a0a1f770bf831d541372313498c66370b
Jackson <jackoson@openshot.org>
2021-06-03 19:16:14 -0500

    Fixed an error that caused the times to be off by 50px

--- src/timeline/js/directives/ruler.js
+++ src/timeline/js/directives/ruler.js
@@ -168,7 +168,7 @@ App.directive("tlRuler", function ($timeout) {
         end = Math.min(scope.scrollLeft + (2*width), $('#ruler').width());
 
         scale = scope.project.scale;
-        for (var i = start - (start % 50) ; i < end; i += 100) {
+        for (var i = start - (start % 100) ; i < end; i += 100) {
           /* create and format span */
           s = $('<span>');
           s.addClass("ruler_time");
