b6fa0b4559dae1c3f30a2ea00170a4bdf68b4c24
Jonathan Thomas <jonathan@openshot.org>
2021-10-25 17:52:22 -0500

    Detect display ratios for multiple screens

--- installer/launch-linux.sh
+++ installer/launch-linux.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
 # Query the current display DPI
-#   This is to fix the AppImage, and scale the UI correctly
-#   for high DPI displays. Ignore this logic if QT_SCREEN_SCALE_FACTOR
-#   is already defined as an environment
+#   Fix the AppImage devicePixelRatio and scale the UI correctly
+#   for High DPI displays. Ignore this logic if QT_SCREEN_SCALE_FACTORS
+#   is already defined as an environment. The format is a
+#   semicolon-separated list of scale factors in the same order as
+#   QGuiApplication::screens().
 if [[ -z "${QT_SCREEN_SCALE_FACTORS}" ]]; then
+
   regex="^.*resolution:\s*([0-9]*)x"
-  DPI_OUTPUT=$(xdpyinfo | grep -B 2 resolution)
-  [[ $DPI_OUTPUT =~ $regex ]]
-  DPI=$(bc <<<"scale=2;${BASH_REMATCH[1]}/92")
-  export QT_SCREEN_SCALE_FACTORS="$DPI"
+  SCREENS=""
+  xdpyinfo | while read -r line ; do
+    # Loop through display results, looking for regex matches
+    [[ $line =~ $regex ]]
+    if [[ -n "${BASH_REMATCH[1]}" ]]; then
+      # Found a DPI results match
+      SCALE_FACTOR=$(bc <<<"scale=2;${BASH_REMATCH[1]}/92")
+      SCREENS+="${SCALE_FACTOR};"
+      echo "Detected scale factor: ${SCREENS}"
+      export QT_SCREEN_SCALE_FACTORS="$SCREENS"
+    fi
+
+  done
 fi
 
 # Add the current folder the library path
