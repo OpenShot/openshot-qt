6a094e84eb9170c8e2a6cd477921bf450933febe
Jonathan Thomas <jonathan@openshot.org>
2022-05-25 16:26:04 -0500

    Adding back in libgobject library, for experimental support of RHEL 84 (which has a custom version of glib)

--- freeze.py
+++ freeze.py
@@ -355,7 +355,7 @@ elif sys.platform == "linux":
                     "libSM.so.6",
                     # Next 5 are all part of glib2
                     #"libglib-2.0.so.0",  <-- experimental for RHEL 84 support (which have a custom version of glib)
-                    "libgobject-2.0.so.0",
+                    #"libgobject-2.0.so.0", <-- experimental for RHEL 84 support (which have a custom version of glib)
                     "libgio-2.0.so.0",
                     "libgmodule-2.0.so.0",
                     "libgthread-2.0.so.0",
