2462717b148d014462db3a6e8e6f21e1500bab4b
Jonathan Thomas <jonathan@openshot.org>
2022-11-03 13:41:52 -0500

    Remove scope.apply from right click directive, to prevent double apply when calling ApplyJsonDiff from context menu

--- src/timeline/js/directives/misc.js
+++ src/timeline/js/directives/misc.js
@@ -33,10 +33,8 @@ App.directive("ngRightClick", function ($parse) {
   return function (scope, element, attrs) {
     var fn = $parse(attrs.ngRightClick);
     element.bind("contextmenu", function (event) {
-      scope.$apply(function () {
-        event.preventDefault();
-        fn(scope, {$event: event});
-      });
+      event.preventDefault();
+      fn(scope, {$event: event});
     });
   };
 });
