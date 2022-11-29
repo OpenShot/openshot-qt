a1dc35dd873f8945b7f847501f110a8218f856b2
Jonathan Thomas <jonathan@openshot.org>
2022-11-28 20:02:40 -0600

    Revert Icon rewrite on AppImage.desktop file

--- installer/build_server.py
+++ installer/build_server.py
@@ -355,8 +355,6 @@ def main():
                 for line in inf:
                     if line.startswith("Exec="):
                         outf.write("Exec=openshot-qt-launch.wrapper %F\n")
-                    elif line.startswith("Icon="):
-                        outf.write("Icon=appimagekit-openshot-qt\n")
                     else:
                         outf.write(line)
 
