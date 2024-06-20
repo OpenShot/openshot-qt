a62a5e6a57525125bdda56b9dad23bd7052d7c95
Jonathan Thomas <jonathan@openshot.org>
2024-06-19 20:30:36 -0500

    Bumping version to 3.2.0, and min libopenshot dependency to 0.3.3.

--- src/classes/info.py
+++ src/classes/info.py
@@ -28,9 +28,9 @@
 import os
 from time import strftime
 
-VERSION = "3.1.1-dev"
-MINIMUM_LIBOPENSHOT_VERSION = "0.3.2"
-DATE = "20230417000000"
+VERSION = "3.2.0"
+MINIMUM_LIBOPENSHOT_VERSION = "0.3.3"
+DATE = "20240619000000"
 NAME = "openshot-qt"
 PRODUCT_NAME = "OpenShot Video Editor"
 GPL_VERSION = "3"
